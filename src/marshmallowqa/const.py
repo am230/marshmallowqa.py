97267db7d0aa9e82925eccdc41b12989e485f27c
2.30am <111672334+am230@users.noreply.github.com>
2024-08-11 00:28:39 +0900

    Update const.py

--- src/marshmallowqa/const.py
+++ src/marshmallowqa/const.py
@@ -1,5 +1,5 @@
 BASE_HEADERS = {
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "user-agent": "am230/marshmallow.py (https://github.com/am230/marshmallow.py)",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "User-Agent": "am230/marshmallow.py (https://github.com/am230/marshmallow.py)",
 }
